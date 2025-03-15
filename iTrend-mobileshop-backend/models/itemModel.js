3cc65a4bc4e17887201dedb06596861878609a73
Parami <paramiashinsana@gmail.com>
2025-03-15 21:18:37 +0530

    updated itemModel

---
+++ iTrend-mobileshop-backend/models/itemModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// Define schema for the item model
+const itemSchema = new mongoose.Schema({
+  type: { 
+    type: String, 
+    required: true, 
+    enum: ['mobile-phone', 'mobile-accessory'], // To distinguish between mobile phones and accessories
+  },
+  brand: { type: String, required: true },
+  model: { type: String, required: true },
+  description: { type: String },
+  color: { type: String },
+  price: { type: Number, required: true },
+  stockQuantity: { type: Number, required: true },
+  status: { 
+    type: String, 
+    required: true, 
+    enum: ['Available', 'Out of Stock'],
+  },
+});
+
+// Ensure unique combination of brand and model
+itemSchema.index({ brand: 1, model: 1 }, { unique: true });
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
