da075a14292bb4152b679425e5fe6d8b059d850e
Parami <paramiashinsana@gmail.com>
2025-03-19 13:35:17 +0530

    updated userModel

--- iTrend-mobileshop-backend/models/userModel.js
+++ iTrend-mobileshop-backend/models/userModel.js
@@ -11,10 +11,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  userId: { type: String, unique: true }, // Unique User ID
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }], // Link to Orders
+  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
+
