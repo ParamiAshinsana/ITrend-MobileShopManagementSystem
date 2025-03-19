a19269edf098b17d9c36ee5f672244d0f92cc8c4
Parami <paramiashinsana@gmail.com>
2025-03-19 12:51:16 +0530

    updated userModel

--- iTrend-mobileshop-backend/models/userModel.js
+++ iTrend-mobileshop-backend/models/userModel.js
@@ -7,9 +7,14 @@
 // }, { timestamps: true });
 
 // module.exports = mongoose.model("User", userSchema);
+
+const mongoose = require("mongoose");
+
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }], // Link to Orders
 }, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
