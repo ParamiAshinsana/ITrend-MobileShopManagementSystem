a67777a95ee72aaf7984f5812243f31d096bf9c6
Parami <paramiashinsana@gmail.com>
2025-03-20 09:41:26 +0530

    update

--- iTrend-mobileshop-backend/index.js
+++ iTrend-mobileshop-backend/index.js
@@ -31,7 +31,6 @@ mongoose
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/items", require("./routes/itemRoutes"));
-app.use("/api/images", require("./routes/imgRoutes")); // Add image routes
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // const PORT = process.env.PORT || 5000;
--- iTrend-mobileshop-backend/models/imgModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const imageSchema = new mongoose.Schema({
-  filename: String,
-  filepath: String, // Store only the file path instead of binary data
-});
-
-module.exports = mongoose.model("Image", imageSchema);
-
